ce6da60efe0ad1c4eb0a27e7fbb02ffc5fc727f0
PicoSupport <eagle.cui@picovr.com>
2019-01-09 13:47:50 +0800

    Initial commit

---
+++ app/src/main/java/com/picovr/killapplication/MainActivity.java
@@ -0,0 +1,63 @@
+package com.picovr.killapplication;
+
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    private static final String TAG = "KillAppActivity";
+    private static final String PACKAGENAME = "com.picovr.gallery";
+    private static int pid = -1;
+//		setprop picovr.factorytest.cmd
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+    public void killAPPClick(View view){
+
+        ActivityManager am = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> mRunningProcess = am.getRunningAppProcesses();
+        Log.e(TAG, mRunningProcess.size() + " ");
+        for (ActivityManager.RunningAppProcessInfo amProcess : mRunningProcess){
+            Log.e(TAG, amProcess.processName);
+            Log.e(TAG, "pid:" + amProcess.pid );
+            if(amProcess.processName.equals(PACKAGENAME)){
+                pid=amProcess.pid;
+                break;
+            }
+        }
+
+        Log.e(TAG, "picovr.factorytest.cmd"+"kill " + pid);
+
+        setSystemProperties("picovr.factorytest.cmd","kill " + pid);
+
+
+    }
+
+    public static void setSystemProperties(String key, String value) {
+        try {
+            final Class<?> systemProperties = Class.forName("android.os.SystemProperties");
+            final Method set = systemProperties.getMethod("set", String.class,
+                    String.class);
+            set.invoke(null, key, value);
+//            LogUtils.i("setSystemProperties--->key=" + key + " value=" + value);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
